a2dc138febd4a279df72a6d5ee7fb297922569aa
SergeyNikolin <48445211+SergeyNikolin@users.noreply.github.com>
2019-05-07 14:31:40 +0300

    Practice completed

--- Практика/NUM_3/Книга и библитотека/Project2/main.cpp
+++ Практика/NUM_3/Книга и библитотека/Project2/main.cpp
@@ -1,7 +1,8 @@
-
-#include<iostream>
 #include"Library.h"
 #include <Windows.h>
+#include <cstdlib>
+#include <iostream>
+#include <clocale>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ int main()
 	for (;;)
 	{
 		system("cls");
-		cout << "1. Добавить новую книгу;" << endl << "2. Посмотреть текущее состояние библиотеки;" << endl << "3. Отсортировать список книг;" << endl << "4. Выйти" << endl << "Номер команды: ";
+		cout << "1. Добавить новую книгу;" << endl << "2. Посмотреть текущее состояние библиотеки;" << endl << "0. Выйти" << endl << "Номер команды: ";
 		int ans;
 		cin >> ans;
 		switch (ans)
@@ -23,15 +24,17 @@ int main()
 			cout << "Заполните форму" << endl << "1.Автор \\ 2.Название книги \\ 3.Кол-во страниц \\ 4.Издатель \\ 5.Год издания" << endl;
 			char author[50], pub[50], name[50]; int year, NOP;
 			cout << "1. ";
-			getline(cin, name);
+			cin.getline(author, '/n');
+			cin.getline(author, '/n');
 			cout << "2. ";
-			getline(cin, name);
+			cin.getline(name, '/n');
 			cout << "3. ";
-			cin >> year;
-			cout << "4. ";
-			getline(cin, name);
-			cout << "5. ";
 			cin >> NOP;
+			cout << "4. ";
+			cin.getline(pub, '/n');
+			cin.getline(pub, '/n');
+			cout << "5. ";
+			cin >> year;
 			Book NewBook(year, NOP, author, pub, name);
 			libra.AddBook(NewBook);
 			break;
@@ -44,25 +47,7 @@ int main()
 			system("pause");
 			break;
 		}
-		case 3:
-		{
-			case3:
-			system("cls");
-			cout << "Выберите способ сортировки: " << endl << "1. Сортировка по автору (по алфавиту);" << endl << "2. Сортировка по названию (по алфавиту);" << endl << "3. Сортировка по названию издателя (по алфавиту);" << endl << "4. Сортировка по дате выпуска (по уменьшению возраста);" << endl << "5. Сортировка по количеству страниц (по возрастанию);" << endl << "Номер команды: ";
-			int ansSort;
-			cin >> ansSort;
-			switch (ansSort)
-			{
-			case 1: { libra.SortByAuthor(); break; }
-			case 2: { libra.SortByName(); break; }
-			case 3: { libra.SortByPub(); break; }
-			case 4: { libra.SortByYear(); break; }
-			case 5: { libra.SortByNOP(); break; }
-			default: { cout << "Вы ввели неправильный номер, попробуйте снова"; Sleep(1500);  goto case3; }
-			}
-			break;
-		}
-		case 4:
+		case 0:
 		{
 			return 0;
 		}
